4654ed4383f9a68b9cee02aa0999f382719edbed
fedeoli <lvofrc95@gmail.com>
2023-08-14 11:37:49 +0300

    chore: align exp setup

--- src/setup/scripts/gcs-jackal.sh
+++ src/setup/scripts/gcs-jackal.sh
@@ -2,13 +2,13 @@
 #export ROS_MASTER_URI=http://132.68.134.213:11311
 #export ROS_IP=132.68.134.213
 
-export ROS_MASTER_URI=http://192.168.43.164:11311
-export ROS_IP=localhost
+#export ROS_MASTER_URI=http://192.168.43.164:11311
+#export ROS_IP=localhost
 
 
 # export ROS_MASTER_URI=http://localhost:11311
 # export ROS_IP=localhost
 
 # fedeoli wifi
-#export ROS_MASTER_URI=http://192.168.43.164:11311
-#export ROS_IP=192.168.43.168
+export ROS_MASTER_URI=http://192.168.43.164:11311
+export ROS_IP=192.168.43.168
